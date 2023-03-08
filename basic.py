22cb0479d701796c3d17099b427ae69eef5974b8
AlphaRant9 <ethan.putz3.14@gmail.com>
2023-03-08 08:23:35 +0000

    Move to LT

--- basic.py
+++ basic.py
@@ -1,21 +1,48 @@
-
 DIGITS = '0123456789'
 
+
 class Error:
-    def __init__(self, error_name, details):
-        self.error_name = error_name
+    def __init__(self, posStart, posEnd, errorName, details):
+        self.posStart = posStart
+        self.posEnd = posEnd
+        self.errorName = errorName
         self.details = details
-    
+
     def asString(self):
-        result = f'{self.error_name}: {self.details}'
+        result = f'{self.errorName}: {self.details}'
+        result += f'\nFile {self.posStart.fn}, line {self.posStart.ln + 1}'
         return result
 
+
 class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
-    
+    def __init__(self, posStart, posEnd, details):
+        super().__init__(posStart, posEnd, 'Illegal Character', details)
+
     def asString(self):
         return super().asString()
+
+
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, currentChar):
+        self.idx += 1
+        self.col += 1
+
+        if currentChar == "\n":
+            self.ln += 1
+            self.col = 0
+
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
 
 TT_INT = 'TT_INT'
 TT_FLOAT = 'TT_FLOAT'
@@ -26,84 +53,96 @@ TT_DIV = 'TT_DIV'
 TT_LPAREN = 'TT_LPAREN'
 TT_RPAREN = 'TT_RPAREN'
 
+
 class Token:
     def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
-        
+
     def __repr__(self):
-        if self.value != None: return f'{self.type}:{self.value}'
-        else: return f'{self.type}'
+        if self.value is not None:
+            return f'{self.type}:{self.value}'
+        else:
+            return f'{self.type}'
+
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, fn, text):
+        self.fn = fn
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1, fn, text)
         self.currentChar = None
         self.advance()
 
     def advance(self):
-        self.pos += 1
-        if (self.pos < len(self.text)): self.currentChar = self.text[self.pos]
-        else: self.currentChar = None
-            
+        self.pos.advance(self.currentChar)
+        if self.pos.idx < len(self.text):
+            self.currentChar = self.text[self.pos.idx]
+        else:
+            self.currentChar = None
+
     def makeTokens(self):
         tokens = []
 
-        while self.currentChar != None:
-            if (self.currentChar in ' \t'):
+        while self.currentChar is not None:
+            if self.currentChar in ' \t':
                 self.advance()
             elif self.currentChar in DIGITS + '.':
                 tokens.append(self.makeNumber())
-            elif (self.currentChar == '+'):
+            elif self.currentChar == '+':
                 tokens.append(Token(TT_PLUS))
                 self.advance()
-            elif (self.currentChar == '-'):
+            elif self.currentChar == '-':
                 tokens.append(Token(TT_MINUS))
                 self.advance()
-            elif (self.currentChar == '*'):
+            elif self.currentChar == '*':
                 tokens.append(Token(TT_MUL))
                 self.advance()
-            elif (self.currentChar == '/'):
+            elif self.currentChar == '/':
                 tokens.append(Token(TT_DIV))
                 self.advance()
-            elif (self.currentChar == '('):
+            elif self.currentChar == '(':
                 tokens.append(Token(TT_LPAREN))
                 self.advance()
-            elif (self.currentChar == ')'):
+            elif self.currentChar == ')':
                 tokens.append(Token(TT_RPAREN))
                 self.advance()
             else:
+                posStart = self.pos.copy()
                 char = self.currentChar
                 self.advance()
-                error = IllegalCharError("'" + char + "'")
+                error = IllegalCharError(posStart, self.pos, "'" + char + "'")
                 return [], error
 
         return tokens, None
-    
+
     def makeNumber(self):
         numStr = ''
         dotCount = 0
 
-        while self.currentChar != None and self.currentChar in DIGITS + '.':
+        while self.currentChar is not None and self.currentChar in DIGITS + '.':
             if self.currentChar == '.':
-                if (dotCount == 1): break
+                if dotCount == 1:
+                    break
                 dotCount += 1
                 numStr += '.'
             else:
                 numStr += self.currentChar
             self.advance()
 
-        if (dotCount == 0):
+        if dotCount == 0:
             return Token(TT_INT, int(numStr))
         else:
             return Token(TT_FLOAT, float(numStr))
 
-def identifyTokens(text):
-    lexer = Lexer(text)
+
+def identifyTokens(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.makeTokens()
 
-    if error: print(error.asString())
-    else: print(tokens)
+    if error:
+        print(error.asString())
+    else:
+        print(tokens)
 
     return tokens, error
--- shell.py
+++ shell.py
@@ -3,7 +3,4 @@ import basic
 while True:
     text = input('basic > ')
     
-    result, error = basic.identifyTokens(text)
-
-    # if error: print(error.as_string())
-    # else: print(result)
+    result, error = basic.identifyTokens('Shell', text)
